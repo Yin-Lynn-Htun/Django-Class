8ab67555b6a2b95f1d8b96fe29b0a658306df18f
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-21 16:16:09 +0630

    Added name for url paths

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index ), # challenges/
-    path('<int:day>/' , views.daily_by_number),
-    path('<str:day>/', views.daily)
+    path('', views.index , name='index'), # challenges/
+    path('<int:day>/' , views.daily_by_number, name="daily_by_number"),
+    path('<str:day>/', views.daily, name="daily")
 ]
